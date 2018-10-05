ec449ac7a5870157aefdc2cef5246b2cd8997405
dirceuvaz <dirceu.info@gmail.com>
2018-10-05 12:31:26 -0300

    Tentando coloca no repositorio remoto

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+echo "<h1>Hello World - Curso na Udemy</h1>";
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="">
+	<title></title>
+	<script type="text/javascript"></script>
+<body>
+	
+</body>
+</head>
+</html>
