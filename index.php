62ab75bec3b81ce1d922b69b1900be86819d7e32
dirceuvaz <dirceu.info@gmail.com>
2018-10-05 12:48:23 -0300

    delete as tags

--- index.php
+++ index.php
@@ -4,15 +4,3 @@ echo "<h1>Hello World - Curso na Udemy</h1>";
 
  ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="">
-	<title></title>
-	<script type="text/javascript"></script>
-<body>
-	
-</body>
-</head>
-</html>
